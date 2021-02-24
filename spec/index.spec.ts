fca0b9518081b4bc4a3403c623a49ff07d17d069
o-su <9551674+o-su@users.noreply.github.com>
2021-02-24 13:55:10 +0100

    added unit tests

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,7 +1,18 @@
 import { UrlBuilder } from "../src/index";
 
-describe("unit tests", () => {
-    test("UrlBuilder returns correct URL for basic configuration", () => {
+describe("UrlBuilder", () => {
+    test("build returns correct URL for minimal configuration", () => {
+        // given
+        const urlBuilder = new UrlBuilder("http");
+
+        // when
+        const result = urlBuilder.build();
+
+        // then
+        expect(result).toEqual("http:");
+    });
+
+    test("build returns correct URL for basic configuration", () => {
         // given
         const urlBuilder = new UrlBuilder("https").setHost("localhost").setPort(8080);
 
@@ -12,7 +23,7 @@ describe("unit tests", () => {
         expect(result).toEqual("https://localhost:8080");
     });
 
-    test("UrlBuilder returns correct URL for full configuration", () => {
+    test("build returns correct URL for full configuration", () => {
         // given
         const urlBuilder = new UrlBuilder("https")
             .setUserInfo("admin", "pass")
@@ -31,6 +42,28 @@ describe("unit tests", () => {
         expect(result).toEqual(
             "https://admin:pass@localhost:8080/dir/subdir?param=value&param2=value2#fragment"
         );
+    });
+
+    test("build returns correct URL without host and port if host is not defined", () => {
+        // given
+        const urlBuilder = new UrlBuilder("https").setPort(8080).addPath("dir");
+
+        // when
+        const result = urlBuilder.build();
+
+        // then
+        expect(result).toEqual("https:dir");
+    });
+
+    test("build returns correct URL without password if only username is defined", () => {
+        // given
+        const urlBuilder = new UrlBuilder("https").setHost("localhost").setUserInfo("admin");
+
+        // when
+        const result = urlBuilder.build();
+
+        // then
+        expect(result).toEqual("https://admin@localhost");
     });
 
     test("clone returns deep copy", () => {
